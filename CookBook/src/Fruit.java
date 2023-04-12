ab3a8f09f7c983027a294b124000bf876406dc14
Andrey5041975 <125498589+Andrey5041975@users.noreply.github.com>
2023-04-12 14:00:18 +0200

    Add files via upload

---
+++ CookBook/src/Fruit.java
@@ -0,0 +1,12 @@
+public class Fruit {
+
+    public String name;
+    public boolean isTasty;
+    public String color;
+    Fruit(String name, boolean isTasty, String color){
+        this.name = name;
+        this.isTasty =isTasty;
+        this.color =color;
+    }
+}
+
